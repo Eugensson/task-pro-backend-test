6dd3456f6b7803af50ce3d7b321fe0aa985b4675
Serhii Kulbashnyi <twitch.live.ua@gmail.com>
2023-08-04 14:27:33 +0300

    Add Regexp for email and password

--- routes/api/email.js
+++ routes/api/email.js
@@ -1,9 +1,18 @@
 const express = require('express');
 
+const { validateBody, authenticate } = require('../../middlewares');
+
+const { sendHelpRequestSchema } = require('../../models/validationSchemas');
+
 const sendHelpRequest = require('../../utils/email/send_help_request');
 
 const router = express.Router();
 
-router.post('/helper', sendHelpRequest);
+router.post(
+  '/helper',
+  authenticate,
+  validateBody(sendHelpRequestSchema),
+  sendHelpRequest
+);
 
 module.exports = router;
