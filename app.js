8ffba3eac50bd7cc1ef72c815ff32e7caab441f6
Serhii Kulbashnyi <twitch.live.ua@gmail.com>
2023-08-05 22:26:26 +0300

    Add Swagger

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
 require('dotenv').config();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 const authRouter = require('./routes/api/auth');
 const boardsRouter = require('./routes/api/boards');
 const emailRouter = require('./routes/api/email');
-const swaggerRouter = require('./routes/api/swagger');
 
 const app = express();
 
@@ -20,7 +21,7 @@ app.use(express.static('public'));
 app.use('/api/users', authRouter);
 app.use('/api/boards', boardsRouter);
 app.use('/api/email', emailRouter);
-app.use('/api/swagger', swaggerRouter);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' });
