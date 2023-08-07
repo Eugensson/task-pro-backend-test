d6c9f7a3d8ee4b209339b5e329ed3db737a594fa
VasilenkoYurii <yura.vasilenko98@gmail.com>
2023-08-07 12:35:18 +0300

    create cloudinary fun

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -1,45 +1,30 @@
+const fs = require('fs');
 const path = require('path');
-const Jimp = require('jimp');
-const fs = require('fs/promises');
-// const { cloudinary } = require('../../utils/cloudinary');
+const cloudinary = require('cloudinary').v2;
 const { ctrlWrapper } = require('../../helpers');
-// const { HttpError, ctrlWrapper } = require('../../helpers');
-
 const { User } = require('../../models/user');
-const avatarsDir = path.join(__dirname, '../', '../', 'public', 'avatars');
+
+const { CLOUDINARY_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET } =
+  process.env;
+
+cloudinary.config({
+  cloud_name: CLOUDINARY_NAME,
+  api_key: CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_API_SECRET,
+});
 
 const updateAvatar = async (req, res) => {
   const { _id } = req.user;
-  const { path: tempUpload, originalname } = req.file;
 
-  const avatarName = `${_id}_${originalname}`;
+  const result = await cloudinary.uploader.upload(req.file.path);
 
-  const resultUpload = path.join(avatarsDir, avatarName);
+  const avatarURL = result.secure_url;
 
-  Jimp.read(tempUpload, (err, avatar) => {
-    if (err) throw err;
-    avatar.resize(250, 250).write(tempUpload, () => {
-      fs.rename(tempUpload, resultUpload);
-      const avatarURL = path.join('avatars', avatarName);
+  User.findByIdAndUpdate(_id, { avatarURL: avatarURL }).then(() => {
+    fs.unlinkSync(req.file.path);
 
-      User.findByIdAndUpdate(_id, { avatarURL }).then(() => {
-        res.json({ avatarURL });
-      });
-    });
+    res.json({ avatarURL });
   });
 };
-
-// const updateAvatar = async (req, res) => {
-//   cloudinary.uploader.upload(req.file.path, function (err, result) {
-//     if (err) {
-//       next(HttpError(500, 'Error'));
-//       console.log(err.message);
-//     }
-//     res.status(200).json({
-//       data: result.url,
-//     });
-//   });
-//   await User.findByIdAndUpdate(_id, { avatarURL: result.url.data });
-// };
 
 module.exports = { updateAvatar: ctrlWrapper(updateAvatar) };
