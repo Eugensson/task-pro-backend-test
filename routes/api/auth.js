f4fcdc0bb0c006c9bb748126a7fa333c81631a35
Serhii Kulbashnyi <twitch.live.ua@gmail.com>
2023-08-07 23:56:06 +0300

    Add Auth routes

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -11,6 +11,4 @@ router.post('/login', validateBody(schemas.loginSchema), ctrl.login);
 
 router.post('/logout', authenticate, ctrl.logout);
 
-router.get('/current', authenticate, ctrl.getCurrent);
-
-module.exports = router
+module.exports = router;
