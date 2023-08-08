7e443de2bc02fe06627138e37ddcef20916d902c
Serhii Kulbashnyi <twitch.live.ua@gmail.com>
2023-08-08 14:25:50 +0300

    Delete dublicate ctrl for update Board

--- controllers/boards/index.js
+++ controllers/boards/index.js
@@ -2,12 +2,10 @@ const { getAll } = require('./getAll');
 const { add } = require('./add');
 const { updateById } = require('./updateById');
 const { deleteById } = require('./deleteById');
-// const { changeBoard } = require('./changeBoard');
 
 module.exports = {
   getAll,
   add,
   updateById,
   deleteById,
-  // changeBoard,
 };
